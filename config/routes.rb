63f85d8bedb3bd9adbe19d4edd40ca8be7599fb7
Slacker87 <ccnp_pro@yahoo.com>
2015-12-17 06:06:06 +0000

    remove unused about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
-  match 'about', to: 'home#about', via: 'get', as: 'about'
   match 'quick', to: 'quick#new', via: 'get', as: 'quick_new'
   match 'quick', to: 'quick#create', via: 'post', as: 'quick_create'
   match 'quick/done', to: 'quick#show', via: 'get', as: 'quick_done'
